02483cf79684566284d757f5495edeb2c3a1ef21
adam seravalle <adam.seravalle@student.tdsb.on.ca>
2016-11-23 21:35:07 -0500

    finished insertion, selection, and bubble sort but not fully commented

--- src/seravalle/Sorts.java
+++ src/seravalle/Sorts.java
@@ -17,70 +17,212 @@ public class Sorts {
 
 	}
 
-	public static void  selectionSort(int[] aInt) {
-        int min =0 ;
-		 for (int i = 0; i < aInt.length;i++) {
-		 min = i;
-		 for (int j = i + 1; j < aInt.length; j++) {
-		 if (aInt[j] < aInt[min]) {
-		 j = min;
-		 }
-		
-		 }
-		 int temp = aInt[i];
-		 aInt[i] = aInt[min];
-		 aInt[min] = temp;
-		
-		
-		 
-		
-		 }
-		 for(int x= 0;x< aInt.length-1;x++){
-			 System.out.println(aInt[x]);
-		 }
-		 
-	
+	/**
+	 * 
+	 * @param arrayInt
+	 */
+	public static void selectionSort(int[] arrayInt) {
+		int min = 0;
+		for (int i = 0; i < arrayInt.length; i++) {
+			min = i;
+			for (int j = i + 1; j < arrayInt.length; j++) {
+				if (arrayInt[j] < arrayInt[min]) {
+					min = j;
+				}
+
+			}
+			int temp = arrayInt[i];
+			arrayInt[i] = arrayInt[min];
+			arrayInt[min] = temp;
+
+			System.out.println(arrayInt[i]);
+		}
+
 	}
 
-	public static int insertionSort(int[] aInt) {
+	/**
+	 * 
+	 * @param arrayInt
+	 */
+	public static void insertionSort(int[] arrayInt) {
 
 		int j;
 		int temp;
-		for (int i = 1; i < aInt.length; i++) {
+		for (int i = 1; i < arrayInt.length; i++) {
 			j = i - 1;
 
-			while (j >= 0 && aInt[j] > aInt[i]) {
-				temp = aInt[i];
-				aInt[i] = aInt[j];
-				aInt[j] = temp;
+			while (j >= 0 && arrayInt[j] > arrayInt[i]) {
+				temp = arrayInt[i];
+				arrayInt[i] = arrayInt[j];
+				arrayInt[j] = temp;
 				i = j;
 				j--;
 			}
 		}
-		for (int x = 0; x < aInt.length; x++) {
-			System.out.println(aInt[x]);
+		for (int x = 0; x < arrayInt.length; x++) {
+			System.out.println(arrayInt[x]);
 		}
 
-		return 0;
 	}
 
-	public static int bubbleSort(int[] aInt) {
+	/**
+	 * 
+	 * @param arrayInt
+	 */
+	public static void bubbleSort(int[] arrayInt) {
 
-		for (int i = 0; i < aInt.length; i++) {
-			for (int j = 1; j < aInt.length - i; j++) {
-				if (aInt[j - 1] > aInt[j]) {
-					int temp = aInt[j - 1];
-					aInt[j - 1] = aInt[j];
-					aInt[j] = temp;
+		for (int i = 0; i < arrayInt.length; i++) {
+			for (int j = 1; j < arrayInt.length - i; j++) {
+				if (arrayInt[j - 1] > arrayInt[j]) {
+					int temp = arrayInt[j - 1];
+					arrayInt[j - 1] = arrayInt[j];
+					arrayInt[j] = temp;
 				}
 			}
 
 		}
-		for (int x = 0; x < aInt.length; x++) {
-			System.out.println(aInt[x]);
+		for (int x = 0; x < arrayInt.length; x++) {
+			System.out.println(arrayInt[x]);
 		}
 
-		return 0;
+	}
+
+	/**
+	 * 
+	 * @param arrayDouble
+	 */
+	public static void selectionSort(double[] arrayDouble) {
+		int min = 0;
+		for (int i = 0; i < arrayDouble.length; i++) {
+			min = i;
+			for (int j = i + 1; j < arrayDouble.length; j++) {
+				if (arrayDouble[j] < arrayDouble[min]) {
+					min = j;
+				}
+
+			}
+			double temp = arrayDouble[i];
+			arrayDouble[i] = arrayDouble[min];
+			arrayDouble[min] = temp;
+
+			System.out.println(arrayDouble[i]);
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param arrayDouble
+	 */
+	public static void insertionSort(double[] arrayDouble) {
+
+		int j;
+		double temp;
+		for (int i = 1; i < arrayDouble.length; i++) {
+			j = i - 1;
+
+			while (j >= 0 && arrayDouble[j] > arrayDouble[i]) {
+				temp = arrayDouble[i];
+				arrayDouble[i] = arrayDouble[j];
+				arrayDouble[j] = temp;
+				i = j;
+				j--;
+			}
+		}
+		for (int x = 0; x < arrayDouble.length; x++) {
+			System.out.println(arrayDouble[x]);
+
+		}
+	}
+
+	/**
+	 * 
+	 * @param arrayDouble
+	 */
+	public static void bubbleSort(double[] arrayDouble) {
+
+		for (int i = 0; i < arrayDouble.length; i++) {
+			for (int j = 1; j < arrayDouble.length - i; j++) {
+				if (arrayDouble[j - 1] > arrayDouble[j]) {
+					double temp = arrayDouble[j - 1];
+					arrayDouble[j - 1] = arrayDouble[j];
+					arrayDouble[j] = temp;
+				}
+			}
+
+		}
+		for (int x = 0; x < arrayDouble.length; x++) {
+			System.out.println(arrayDouble[x]);
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param arrayString
+	 */
+	public static void selectionSort(String[] arrayString) {
+		int min = 0;
+		for (int i = 0; i < arrayString.length; i++) {
+			min = i;
+			for (int j = i + 1; j < arrayString.length; j++) {
+				if (arrayString[j].compareTo(arrayString[min]) < 0) {
+					min = j;
+				}
+
+			}
+			String temp = arrayString[i];
+			arrayString[i] = arrayString[min];
+			arrayString[min] = temp;
+
+			System.out.println(arrayString[i]);
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param arrayString
+	 */
+	public static void insertionSort(String[] arrayString) {
+
+		int j;
+		String temp;
+		for (int i = 1; i < arrayString.length; i++) {
+			j = i - 1;
+
+			while (j >= 0 && arrayString[j].compareTo(arrayString[i]) > 0) {
+				temp = arrayString[i];
+				arrayString[i] = arrayString[j];
+				arrayString[j] = temp;
+				i = j;
+				j--;
+			}
+		}
+		for (int x = 0; x < arrayString.length; x++) {
+			System.out.println(arrayString[x]);
+
+		}
+	}
+	public static void bubbleSort(String[] arrayString) {
+
+		for (int i = 0; i < arrayString.length; i++) {
+			for (int j = 1; j < arrayString.length - i; j++) {
+				if (arrayString[j - 1].compareTo(arrayString[j])> 0) {
+					String temp = arrayString[j - 1];
+					arrayString[j - 1] = arrayString[j];
+					arrayString[j] = temp;
+				}
+			}
+
+		}
+		for (int x = 0; x < arrayString.length; x++) {
+			System.out.println(arrayString[x]);
+		}
+
+	}
+
+	private static void mergeSort() {
 
 	}
 }
--- src/seravalle/testSort.java
+++ src/seravalle/testSort.java
@@ -4,10 +4,28 @@ public class testSort {
 
 	public static void main(String[] args) {
 
-		int[] aInt = { 42, 7, 1, 2, 9, 4, 14, 3, 29, 20 };
+		int[] arrayInt = { 42, 7, 1, 2, 9, 4, 14, 3, 29, 20 };
 
-		Sorts.selectionSort(aInt);
-
+		System.out.println("Selection sort: integer");
+		Sorts.selectionSort(arrayInt);
+		System.out.println("Insertion sort: integer");
+		Sorts.insertionSort(arrayInt);
+		System.out.println("Bubble sort: integer");
+		Sorts.bubbleSort(arrayInt);
+		double[] arrayDouble = { 42.3, 7.5, 1.0, 2.9, 9.4, 4.6, 14.7, 3.1, 29.3, 20.2 };
+		System.out.println("Selection sort: double");
+		Sorts.selectionSort(arrayDouble);
+		System.out.println("Insertion sort: double");
+		Sorts.insertionSort(arrayDouble);
+		System.out.println("Bubble sort: double");
+		Sorts.bubbleSort(arrayDouble);
+		String[] arrayString = { "j", "a", "h", "g", "e", "c", "i", "f", "d", "b" };
+		System.out.println("Selection sort: string");
+		Sorts.selectionSort(arrayString);
+		System.out.println("Insertion sort: string");
+		Sorts.insertionSort(arrayString);
+		System.out.println("Bubble sort: string ");
+		Sorts.bubbleSort(arrayString);
 	}
 
 }
