cc5f3d77e4af8483123af4c6168a904e0035ad28
adam seravalle <adam.seravalle@student.tdsb.on.ca>
2017-01-05 12:33:26 -0500

    test push

--- src/seravalle/Sorts.java
+++ src/seravalle/Sorts.java
@@ -1,10 +1,14 @@
 package seravalle;
 
+
+
 /**
  * This program sorts an int, double, and string array by selection sort ,
  * insertion sort , and bubble sort
  * 
- * @author Adam Seravalle Sorts.java November 14 2016
+ * @author Adam Seravalle 
+ * Sorts.java 
+ * November 14 2016
  */
 public class Sorts {
 	/**
@@ -18,6 +22,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts an array of ints by selection sort
 	 * 
 	 * @param arrayInt
 	 */
@@ -41,6 +46,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts and array of ints by insertion sort
 	 * 
 	 * @param arrayInt
 	 */
@@ -66,6 +72,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts an array of ints by bubble sort
 	 * 
 	 * @param arrayInt
 	 */
@@ -88,6 +95,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts an array of doubles by selection sort
 	 * 
 	 * @param arrayDouble
 	 */
@@ -111,6 +119,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts an array of doubles by insertion sort
 	 * 
 	 * @param arrayDouble
 	 */
@@ -136,6 +145,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts an array of doubles by bubble sort
 	 * 
 	 * @param arrayDouble
 	 */
@@ -158,6 +168,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts an array of strings by selection sort
 	 * 
 	 * @param arrayString
 	 */
@@ -181,6 +192,7 @@ public class Sorts {
 	}
 
 	/**
+	 * Sorts an array of strings by insertion sort
 	 * 
 	 * @param arrayString
 	 */
@@ -204,11 +216,17 @@ public class Sorts {
 
 		}
 	}
+
+	/**
+	 * Sorts an array of strings by bubble sort
+	 * 
+	 * @param arrayString
+	 */
 	public static void bubbleSort(String[] arrayString) {
 
 		for (int i = 0; i < arrayString.length; i++) {
 			for (int j = 1; j < arrayString.length - i; j++) {
-				if (arrayString[j - 1].compareTo(arrayString[j])> 0) {
+				if (arrayString[j - 1].compareTo(arrayString[j]) > 0) {
 					String temp = arrayString[j - 1];
 					arrayString[j - 1] = arrayString[j];
 					arrayString[j] = temp;
@@ -222,7 +240,450 @@ public class Sorts {
 
 	}
 
-	private static void mergeSort() {
+	
+
+	/**
+	 * This method takes a array and splits it up continuously
+	 * @param arrayInt 
+	 * @param start 
+	 * @param end 
+	 */
+	private static void mergeSort(int arrayInt[], int start, int end)
+	{
+		if(start<end)
+		{
+			int mid = (end + start) / 2;
+			mergeSort(arrayInt, start, mid);
+			mergeSort(arrayInt, mid+1, end);
+			merge(arrayInt, start, mid, end);
+		}
+	}
+	/**
+	 * This method sets up values for merge sort
+	 * 
+	 * @param arrayInt
+	 *            
+	 * @return 
+	 */
+	public static int[] mergeSort(int[] arrayInt) {
+		int start = 0;
+		int end = arrayInt.length - 1;
+		mergeSort(arrayInt, start, end);
+		return arrayInt;
+	}
+
+	/**
+	 * 
+	 * @param arrayInt
+	 *           
+	 * @param start
+	 *           
+	 * @param mid
+	 *           
+	 * @param end
+	 *            
+	 */
+	public static void merge(int[] arrayInt, int start, int mid, int end) {
+		int[] temp = new int[arrayInt.length];
+		for (int i = 0; i < arrayInt.length; i++) {
+			temp[i] = arrayInt[i];
+		}
+		int count = start;
+		int num = mid + 1;
+		int i = start;
+		while (count <= mid && num <= end) {
+			if (temp[count] <= temp[num]) {
+				arrayInt[i] = temp[count];
+				count++;
+
+			} else {
+				arrayInt[i] = temp[num];
+				num++;
+			}
+			i++;
+		}
+		while (count <= mid) {
+			arrayInt[i] = temp[count];
+			i++;
+			count++;
+		}
+		while (num <= end) {
+			arrayInt[i] = temp[num];
+			i++;
+			num++;
+		}
+	}
+
+	/**
+	 * This method takes a array and splits it up continuously
+	 * @param arrayDouble 
+	 * @param start 
+	 * @param end 
+	 */
+	private static void mergeSort(double arrayDouble[], int start, int end)
+	{
+		if(start<end)
+		{
+			int mid = (end + start) / 2;
+			mergeSort(arrayDouble, start, mid);
+			mergeSort(arrayDouble, mid+1, end);
+			merge(arrayDouble, start, mid, end);
+		}
+	
+	}
+	/**
+	 * This method sets up values for merge sort
+	 * 
+	 * @param arrayDouble
+	 *            
+	 * @return 
+	 */
+	public static double[] mergeSort(double[] arrayDouble) {
+		int start = 0;
+		int end = arrayDouble.length - 1;
+		mergeSort(arrayDouble, start, end);
+		return arrayDouble;
+	}
+
+	/**
+	 * 
+	 * @param arrayDouble
+	 *           
+	 * @param start
+	 *           
+	 * @param mid
+	 *           
+	 * @param end
+	 *            
+	 */
+	public static void merge(double[] arrayDouble, int start, int mid, int end) {
+		double[] temp = new double[arrayDouble.length];
+		for (int i = 0; i < arrayDouble.length; i++) {
+			temp[i] = arrayDouble[i];
+		}
+		int count = start;
+		int num = mid + 1;
+		int i = start;
+		while (count <= mid && num <= end) {
+			if (temp[count] <= temp[num]) {
+				arrayDouble[i] = temp[count];
+				count++;
+
+			} else {
+				arrayDouble[i] = temp[num];
+				num++;
+			}
+			i++;
+		}
+		while (count <= mid) {
+			arrayDouble[i] = temp[count];
+			i++;
+			count++;
+		}
+		while (num <= end) {
+			arrayDouble[i] = temp[num];
+			i++;
+			num++;
+		}
+	}
+	
+	
+	/**
+	 * This method takes a array and splits it up continuously
+	 * @param arrayDouble 
+	 * @param start 
+	 * @param end 
+	 */
+	private static void mergeSort(String arrayString[], int start, int end)
+	{
+		if(start<end)
+		{
+			int mid = (end + start) / 2;
+			mergeSort(arrayString, start, mid);
+			mergeSort(arrayString, mid+1, end);
+			merge(arrayString, start, mid, end);
+		}
+	
+	}
+	/**
+	 * This method sets up values for merge sort
+	 * 
+	 * @param arrayDouble
+	 *            
+	 * @return 
+	 */
+	public static String[] mergeSort(String[] arrayString) {
+		int start = 0;
+		int end = arrayString.length - 1;
+		mergeSort(arrayString, start, end);
+		return arrayString;
+	}
+
+	/**
+	 * 
+	 * @param arrayString
+	 * @param start 
+	 * @param mid 
+	 * @param end 
+	 */
+	private static void merge(String[] arrayString,int start, int mid, int end )
+	{
+		String [] temp = new String[arrayString.length];
+		for (int i = 0; i < arrayString.length; i++)
+		{
+			temp[i] = arrayString[i];
+		}
+		int count = start;
+		int num = mid+1;
+		int i = start;
+		while (count <= mid && num <= end)
+		{
+			if (temp[count].compareTo(temp[num]) <= 0) 
+			{
+				arrayString[i] =temp[count];
+				count++;
+				
+			} 
+			else
+			{
+				arrayString[i] = temp[num];
+				num++;
+			}
+			i++;
+		}
+		while (count <= mid)
+		{
+			arrayString[i] = temp[count];
+			i++;
+			count++;
+		}
+		while(num <= end)
+		{
+			arrayString[i] = temp[num];
+			i++;
+			num++;
+		}
+	}
+	
+	
+	/**
+	 * This method takes an array and gets values ready for quicksort.
+	 * @param arrayInt
+	 * @return 
+	 */
+	public static int[] quickSort(int []arrayInt)
+	{	
+		int x=0;
+		int y=arrayInt.length-1;
+		quickSort(arrayInt, x, y);
+		return arrayInt;
 
 	}
-}
+	/**
+	 * This method takes an array of ints and sorts its values recursively using a helper method
+	 * @param arrayInt
+	 * @param x
+	 * @param y 
+	 */
+	private static void quickSort(int arrayInt[], int x, int y)
+	{
+
+		int num = partition(arrayInt, x, y);
+
+		if (x< num - 1)
+		{
+			quickSort(arrayInt, x, num - 1);
+		}
+		if (num < y)
+		{
+			quickSort(arrayInt, num,y);
+			
+		}
+
+	}
+	/**
+	 * This method takes an array, and sorts it using the quicksort method
+	 * @param arrayInt
+	 * @param x 
+	 * @param y
+	 * @return 
+	 */
+	private static int partition (int arrayInt[], int x, int y)
+	{
+
+		int  mid= (x+y)/2;
+		int pivot = arrayInt[mid];
+
+		while (x <= y) 
+		{
+
+			while (arrayInt[x] < pivot)
+			{
+				x++;
+			}
+			while (arrayInt[y] > pivot)
+			{
+				y= y-1;
+			}
+			if (x <= y) 
+			{
+				int temp=arrayInt[y];
+				arrayInt[y]=arrayInt[x];
+				arrayInt[x]=temp;		
+				x++;
+				y= y-1;
+			}
+		}
+
+		return x;
+	
+	}
+	/**
+	 * This method takes an array of doubles and gets values ready for quicksort.
+	 * @param arrayDouble
+	 * @return 
+	 */
+	public static double[] quickSort(double []arrayDouble)
+	{	
+		int x=0;
+		int y=arrayDouble.length-1;
+		quickSort(arrayDouble, x, y);
+		return arrayDouble;
+
+	}
+	/**
+	 * This method takes an array of doubles and sorts its values recursively using a helper method
+	 * @param arrayDouble
+	 * @param x 
+	 * @param y 
+	 */
+	private static void quickSort(double []arrayDouble, int x, int y)
+	{
+
+		int num = partition(arrayDouble, x, y);
+
+		if (x< num - 1)
+		{
+			quickSort(arrayDouble, x, num - 1);
+		}
+		if (num < y)
+		{
+			quickSort(arrayDouble, num,y);
+			
+		}
+
+	}
+	/**
+	 * This method takes an array of doubles, and sorts it using the quicksort method
+	 * @param arrayDouble 
+	 * @param x 
+	 * @param y 
+	 * @return 
+	 */
+	private static int partition (double []arrayDouble, int x, int y)
+	{
+
+		int  mid= (x+y)/2;
+		double pivot = arrayDouble[mid];
+
+		while (x <= y) 
+		{
+
+			while (arrayDouble[x] < pivot)
+			{
+				x++;
+			}
+			while (arrayDouble[y] > pivot)
+			{
+				y= y-1;
+			}
+			if (x <= y) 
+			{
+				double temp=arrayDouble[y];
+				arrayDouble[y]=arrayDouble[x];
+				arrayDouble[x]=temp;		
+				x++;
+				y= y-1;
+			}
+		}
+
+		return x;
+	
+	}
+	/**
+	 * This method takes an array of strings and gets values ready for quicksort.
+	 * @param arrayString 
+	 * @return 
+	 */
+	public static String[] quickSort(String []arrayString)
+	{	
+		int x=0;
+		int y=arrayString.length-1;
+		quickSort(arrayString, x, y);
+		return arrayString;
+
+	}
+	/**
+	 * This method takes an array of strings and sorts its values recursively using a helper method
+	 * @param arrayString 
+	 * @param x 
+	 * @param y 
+	 */
+	private static void quickSort(String []arrayString, int x, int y)
+	{
+
+		int num = partition(arrayString, x, y);
+
+		if (x< num - 1)
+		{
+			quickSort(arrayString, x, num - 1);
+		}
+		if (num < y)
+		{
+			quickSort(arrayString, num,y);
+			
+		}
+
+	}
+	/**
+	 * This method takes an array of strings , and sorts it using the quicksort method
+	 * @param arrayString 
+	 * @param x 
+	 * @param y 
+	 * @return 
+	 */
+	private static int partition (String arrayString[], int x, int y)
+	{
+
+		int  mid= (x+y)/2;
+		String pivot = arrayString[mid];
+
+		while (x <= y) 
+		{
+
+			while (arrayString[x].compareTo(pivot)<0)
+			{
+				x++;
+			}
+			while (arrayString[y].compareTo(pivot) > 0)
+			{
+				y= y-1;
+			}
+			if (x <= y) 
+			{
+				String temp=arrayString[y];
+				arrayString[y]=arrayString[x];
+				arrayString[x]=temp;		
+				x++;
+				y= y-1;
+			}
+		}
+
+		return x;
+	
+	}
+	}
+
+
+	
+
--- src/seravalle/testSort.java
+++ src/seravalle/testSort.java
@@ -1,5 +1,10 @@
 package seravalle;
-
+/**
+ * sorts arrays with different methods 
+ * testSort.java
+ * @author Adam Seravalle
+ * November 16 2016
+ */
 public class testSort {
 
 	public static void main(String[] args) {
@@ -12,6 +17,17 @@ public class testSort {
 		Sorts.insertionSort(arrayInt);
 		System.out.println("Bubble sort: integer");
 		Sorts.bubbleSort(arrayInt);
+		System.out.println("Merge sort: integer ");
+		Sorts.mergeSort(arrayInt);
+		for (int i = 0; i < arrayInt.length; i++) {
+			System.out.println(arrayInt[i]);
+		}
+		System.out.println("Quick sort: integer ");
+		Sorts.quickSort(arrayInt);
+		for (int i = 0; i < arrayInt.length; i++) {
+			System.out.println(arrayInt[i]);
+		}
+
 		double[] arrayDouble = { 42.3, 7.5, 1.0, 2.9, 9.4, 4.6, 14.7, 3.1, 29.3, 20.2 };
 		System.out.println("Selection sort: double");
 		Sorts.selectionSort(arrayDouble);
@@ -19,6 +35,16 @@ public class testSort {
 		Sorts.insertionSort(arrayDouble);
 		System.out.println("Bubble sort: double");
 		Sorts.bubbleSort(arrayDouble);
+		System.out.println("Merge sort: double ");
+		Sorts.mergeSort(arrayDouble);
+		for (int j = 0; j < arrayDouble.length; j++) {
+			System.out.println(arrayDouble[j]);
+		}
+		System.out.println("Quick sort: double ");
+		Sorts.quickSort(arrayDouble);
+		for (int i = 0; i < arrayDouble.length; i++) {
+			System.out.println(arrayDouble[i]);
+		}
 		String[] arrayString = { "j", "a", "h", "g", "e", "c", "i", "f", "d", "b" };
 		System.out.println("Selection sort: string");
 		Sorts.selectionSort(arrayString);
@@ -26,6 +52,16 @@ public class testSort {
 		Sorts.insertionSort(arrayString);
 		System.out.println("Bubble sort: string ");
 		Sorts.bubbleSort(arrayString);
-	}
+		System.out.println("Merge sort: string ");
+		Sorts.mergeSort(arrayString);
+		for (int x = 0; x < arrayString.length; x++) {
+			System.out.println(arrayString[x]);
+		}
+		System.out.println("Quick sort: string ");
+		Sorts.quickSort(arrayString);
+		for (int i = 0; i < arrayString.length; i++) {
+			System.out.println(arrayString[i]);
+		}
 
+	}
 }
